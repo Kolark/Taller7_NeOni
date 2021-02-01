d2481ab7e89b6d3db5c89bd2617408dbf9fe4380
Kolark <47009873+Kolark@users.noreply.github.com>
2021-02-01 10:31:58 -0500

    InputController y Hierarchy2
    Añadí una primera versión del InputController ,ademas se añadio el paquete de hierarchy2 que se me habia olvidado.

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.InputSystem;
+/// <summary>
+/// Clase que funciona para obtener los inputs del jugador a traves de eventos.
+/// Unirse el en start a traves del singleton, si se unen en el awake tirara problema
+/// </summary>
+public class InputController : MonoBehaviour
+{
+    private static InputController instance;
+    public static InputController Instance { get => instance; }
+    /// <summary>
+    /// Evento Salto
+    /// </summary>
+    public Action Jump;
+    /// <summary>
+    /// Evento Ataque
+    /// </summary>
+    public Action Attack;
+
+    private Vector2 move;
+    /// <summary>
+    /// Vector 2 de joystick|WASD
+    /// </summary>
+    public Vector2 Move { get => move;}
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(this);
+        }
+        instance = this;
+    }
+
+    public void OnMove(InputValue value)
+    {
+        move = value.Get<Vector2>();
+    }
+
+    public void OnJump()
+    {
+        Jump?.Invoke();
+    }
+
+    public void OnAttack()
+    {
+        Attack?.Invoke();
+    }
+}
