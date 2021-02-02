9aa3d6f69330af958a271d00b67f3a6ce0658f07
ZeritFreeman <juanc.sierram@upb.edu.co>
2021-02-02 17:58:15 -0500

    Movimiento Basico del Personaje
    Se agrego la escena CharacterMovement. Con un player funcional.
    
    Puede moverse horizontalmente y saltar. Aparte su animator controller ya esta funcionando.

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,156 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This script is responsable for the basic character movement
+/// It has a referece to the character animator controller and 5 rays that
+/// check if the character is grounded.
+/// The movement is done by a character controller
+/// </summary>
+
+public class CharacterMovement : MonoBehaviour
+{
+    [SerializeField] private Animator anim;
+    private CharacterController controller;
+    private Vector3 slopeNormal;
+    private bool grounded;
+    private bool facingRight = true;
+    private float verticalVelocity;
+
+    InputController inputController;
+
+    [SerializeField] private float speedX = 5f;
+    [SerializeField] private float speedY = 5f;
+    [SerializeField] private float gravity = 0.25f;
+    [SerializeField] private float terminalVelocity = 5f;
+    [SerializeField] private float jumpForce = 8f;
+
+    [SerializeField] private float extremitiesOffset = 0.05f;
+    [SerializeField] private float innerVerticalOffset = 0.25f;
+    [SerializeField] private float distanceGrounded = 0.15f;
+    [SerializeField] private float slopeThreshold = 0.55f;
+
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Start()
+    {
+        inputController = InputController.Instance;
+        inputController.Jump += Jump;
+    }
+
+    private void Update()
+    {
+        Vector3 inputVector = PoolInput();
+        Vector3 moveVector = new Vector3(inputVector.x * speedX, 0, inputVector.y * speedY); ;
+
+        if (moveVector.x > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if (moveVector.x < 0 && facingRight)
+        {
+            Flip();
+        }
+
+        anim?.SetFloat("Speed", moveVector.magnitude);
+        grounded = Grounded();
+        anim?.SetBool("Grounded", grounded);
+        if (grounded)
+        {
+            verticalVelocity = -1;
+        }
+        else
+        {
+            verticalVelocity -= gravity;
+            slopeNormal = Vector3.up;
+
+            if (verticalVelocity < -terminalVelocity)
+                verticalVelocity = -terminalVelocity;
+        }
+
+        moveVector.y = verticalVelocity;
+        anim?.SetFloat("VerticalVelocity", verticalVelocity);
+
+        if (slopeNormal != Vector3.up) moveVector = FollowFloor(moveVector);
+
+        controller.Move(moveVector * Time.deltaTime);
+    }
+    private Vector3 PoolInput()
+    {
+        Vector3 r = Vector3.zero;
+        r.x = inputController.Move.x;
+        //r.y = inputController.Move.y;
+
+        return r.normalized;
+    }
+    private Vector3 FollowFloor(Vector3 _moveVector)
+    {
+        Vector3 right = new Vector3(slopeNormal.y, -slopeNormal.x, 0).normalized;
+        Vector3 forward = new Vector3(0, -slopeNormal.z, slopeNormal.y).normalized;
+        return right * _moveVector.x + forward * _moveVector.z;
+    }
+    public bool Grounded()
+    {
+        if (verticalVelocity > 0)
+            return false;
+
+        float yRay = (controller.bounds.center.y - (controller.height * 0.5f)) + innerVerticalOffset;
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(new Vector3(controller.bounds.center.x, yRay, controller.bounds.center.z), -Vector3.up, out hit, innerVerticalOffset + distanceGrounded))
+        {
+            Debug.DrawRay(new Vector3(controller.bounds.center.x, yRay, controller.bounds.center.z), -Vector3.up * (innerVerticalOffset + distanceGrounded), Color.red);
+            slopeNormal = hit.normal;
+            return (slopeNormal.y > slopeThreshold) ? true : false;
+        }
+
+        if (Physics.Raycast(new Vector3(controller.bounds.center.x + (controller.bounds.extents.x - extremitiesOffset), yRay, controller.bounds.center.z + (controller.bounds.extents.z - extremitiesOffset)), -Vector3.up, out hit, innerVerticalOffset + distanceGrounded))
+        {
+            slopeNormal = hit.normal;
+            return (slopeNormal.y > slopeThreshold) ? true : false;
+        }
+
+        if (Physics.Raycast(new Vector3(controller.bounds.center.x - (controller.bounds.extents.x - extremitiesOffset), yRay, controller.bounds.center.z + (controller.bounds.extents.z - extremitiesOffset)), -Vector3.up, out hit, innerVerticalOffset + distanceGrounded))
+        {
+            slopeNormal = hit.normal;
+            return (slopeNormal.y > slopeThreshold) ? true : false;
+        }
+
+        if (Physics.Raycast(new Vector3(controller.bounds.center.x + (controller.bounds.extents.x - extremitiesOffset), yRay, controller.bounds.center.z - (controller.bounds.extents.z - extremitiesOffset)), -Vector3.up, out hit, innerVerticalOffset + distanceGrounded))
+        {
+            slopeNormal = hit.normal;
+            return (slopeNormal.y > slopeThreshold) ? true : false;
+        }
+
+        if (Physics.Raycast(new Vector3(controller.bounds.center.x - (controller.bounds.extents.x - extremitiesOffset), yRay, controller.bounds.center.z - (controller.bounds.extents.z - extremitiesOffset)), -Vector3.up, out hit, innerVerticalOffset + distanceGrounded))
+        {
+            slopeNormal = hit.normal;
+            return (slopeNormal.y > slopeThreshold) ? true : false;
+        }
+
+        return false;
+    }
+
+    private void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+    private void Jump()
+    {
+        if (grounded)
+        {
+            verticalVelocity = jumpForce;
+            slopeNormal = Vector3.up;
+            anim?.SetTrigger("Jump");
+        }
+    }
+}
