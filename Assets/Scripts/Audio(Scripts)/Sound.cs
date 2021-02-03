d8a072c141af285cdd34de312e944c6a173fab0b
Kolark <47009873+Kolark@users.noreply.github.com>
2021-02-02 20:46:35 -0500

    Primera Versión del AudioManager
    Cree la primera versión del audioManager. Se trata de un prefab que contiene el script, implemente el Singleton, y trata a los sonidos de 2 maneras, aquellos que estan presente en la mayoria  de las escenas y los externos que son especificos de una escena.

---
+++ Assets/Scripts/Audio(Scripts)/Sound.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+[System.Serializable]
+public class Sound
+{
+    public string name;
+    public AudioClip clip;
+    public AudioMixerGroup group;
+    [Range(0, 1f)]
+    public float volume;
+    [Range(.1f, 3f)]
+    public float pitch = 1;
+    public bool loop;
+    [HideInInspector]
+    public AudioSource source;
+    /// <summary>
+    /// Ayuda a organizar los parámetros de esta clase, ademas del audioSource
+    /// </summary>
+    /// <param name="source"></param>
+    public void SetAudioSource(ref AudioSource source)
+    {
+        this.source = source;
+        this.source.clip = clip;
+        this.source.volume = volume;
+        this.source.pitch = pitch;
+        this.source.loop = loop;
+        this.source.outputAudioMixerGroup = group;
+    }
+}
