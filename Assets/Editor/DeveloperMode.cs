46f46c392fe9322974459728d512945b4051d3a2
danielg-r <d4nielg.r@gmail.com>
2021-02-04 16:19:53 -0500

    DelevoperMode
    - Agregado framework inicial para el modo desarrollador, por ahora sin funciones que se agregarán después cuando se necesiten.

---
+++ Assets/Editor/DeveloperMode.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+
+public class DeveloperMode : MonoBehaviour {
+
+    //This script should methods that reference static controllers/managers
+    //and be able to modify them to grant developers an advantage in terms of testing.
+
+    [UnityEditor.MenuItem("Tools/Information")]
+    public static void TestFunction() {
+        Debug.Log("This is a test developer function.");
+    }
+
+}
